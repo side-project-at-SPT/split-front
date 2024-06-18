0f097933f57e73c29edd059b11c88f676b4e25d7
Parsons <gongyingparsons@gmail.com>
2024-06-18 14:42:40 +0800

    lint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,37 @@
 <script setup>
+import {
+  onMounted, ref 
+} from 'vue'
 import api from '@/assets/api'
 import { RouterLink } from 'vue-router'
+const isLogin = ref(false)
+const rooms = ref([
+
+])
 const getRooms = async () => {
-  return api.getRooms()
+  const data = await api.getRooms()
+  rooms.value = data.rooms
+  return
 }
 const login = async () => {
-  return api.login('account', 'password')
+  const data = await api.login('account', 'password')
+  if (data.token) {
+    isLogin.value = true
+    getRooms()
+  }
+  return
 }
+const handleJoinRoom = async (room) => {
+  alert(`join room ${ room.id }`)
+  // return api.joinRoom(room.id)
+}
+onMounted(() => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    isLogin.value = true
+    getRooms()
+  }
+})
 </script>
 
 <template>
@@ -15,12 +40,39 @@ const login = async () => {
       Game
     </RouterLink>
   </div>
-  <div>
-    <div @click="getRooms">
-      取得房間列表
-    </div>
+  <div v-if="!isLogin">
     <div @click="login">
       登入
     </div>
   </div>
+  <div
+    v-if="isLogin"
+    class="flex gap-2 flex-warp"
+  >
+    <div
+      v-for="room in rooms"
+      :key="room.id"
+      class="text-center border-2 border-blue-300 w-[150px] h-[150px] bg-blue-50 rounded-md cursor-pointer flex justify-center hexagon-ice items-center hover:scale-105 transition-transform duration-300"
+      @click="handleJoinRoom(room)"
+    >
+      <div class="py-2">
+        {{ room.name }}
+      </div>
+    </div>
+    <div class="w-[150px] h-[150px] new-room flex items-center justify-center cursor-pointer hover:scale-105 transition-transform duration-300 ">
+      新增房間
+    </div>
+  </div>
 </template>
+
+<style scoped>
+  .hexagon-ice {
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    background-image: linear-gradient(to bottom right, #b3d9ff, #218ed3);
+  }
+
+  .new-room {
+    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+    background-image: linear-gradient(to bottom right, #cae5ff, #87b0ca);
+  }
+</style>

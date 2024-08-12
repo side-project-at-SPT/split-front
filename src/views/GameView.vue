6ec7e255c5bd4e2edf02d1e006a9ca10b832b337
Parsons <gongyingparsons@gmail.com>
2024-08-12 19:04:52 +0800

    企鵝移動位置調整

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -348,7 +348,7 @@ const moveItem = ({
   // 在 pasture-table 新增一個羊
   const newDiv = document.createElement('div')
   newDiv.style.left = `calc(${ from.x * 105 }px + ${ from.y * 105 }px * sin(30deg) + 25px)`
-  newDiv.style.top = `calc(${ from.y * 105 }px * cos(30deg) + 25px)`
+  newDiv.style.top = `calc(${ from.y * 105 }px * cos(30deg) + 45px)`
   newDiv.style.position = 'absolute'
   newDiv.style.width = '50px'
   newDiv.style.height = '50px'
